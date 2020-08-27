08734019d8cde13e6919e6c0e847e760f5aa72e5
tongyue <tongyue_pm@163.com>
2020-08-27 20:29:24 +0800

    第一次向githubt提交项目

---
+++ demo01.py
@@ -0,0 +1,29 @@
+
+# print("Hello World")  #字符串
+# print(123456)  #整数
+# print(True)  #python demo01.py布尔值
+# print(())  #元组
+# print([])  #数组
+# print({})  #字典
+
+# print("hhhhh",1234)
+# print("哈"*10)
+# print("hahha"+"链接") #字符串的链接
+# print(2>3)
+# print(2<3)
+# a = float(input("请输入a："))
+# b = float(input("请输入b："))
+# print("输出input获取的内容:",a+b)
+
+# print(type(23333))
+# print(type("hhhhh"))
+# print(type(False))
+
+# a =  ("233")
+# print(type(a))
+# print(len(a))
+ 
+#获取两段内容并计算他们长度的和
+a = input("输入字符串a:")
+b = input("输入字符串b:")
+print(len(a + b))
